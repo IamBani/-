cd198ec8632bb454b8588bcb4d58756ecb44a2d9
unknown <13972186114@139.com>
2019-10-30 11:35:33 +0800

    222

---
+++ src/components/contacts.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="firder">
+        <ul class="habox" @click="search">
+            <li class="iamge"><img src="../assets/2.jpg" alt=""></li>
+            <li class="newfir">新的朋友</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default{
+    name:"contacts",
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        search(){
+            this.$router.push({path:'/search'})
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .firder{
+        display: flex;
+        flex-direction: column;
+        >.habox{
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            padding: 10px;
+            box-sizing: border-box;
+            >.iamge img{
+                width: 50px;
+            }
+            >.newfir{
+                margin-left: 10px;
+            }
+        }
+    }
+
+</style>
